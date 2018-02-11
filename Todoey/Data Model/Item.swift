261b49f794124513e0ac6625702a98ee5ac9c352
Patrick Quilty <pquilty79@gmail.com>
2018-02-11 15:38:08 -0600

    Convert to MVC design patter

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Patrick Quilty on 2/11/18.
+//  Copyright © 2018 Patrick Quilty. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
